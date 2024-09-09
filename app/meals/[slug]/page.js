412e6a2b4ed1af2727d56811e04cb42f93e90e37
Pahasara Ridmaka <pridmaka@gmail.com>
2024-09-09 11:14:55 +0530

    Add image picker

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,7 +1,36 @@
-export default function SlugRoute() {
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+export default function SlugRoute({ params }) {
+  const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "</br>");
   return (
-    <div className="">
-      <h2>Slug Route</h2>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill alt={meal.title} src={meal.image} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
